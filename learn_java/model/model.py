3e0ff2917ab42df3f34e0dc72308cecd66c6a062
Alexander Goebel <agoebel@mailbox.org>
2024-08-26 21:30:59 +0200

    second commit

--- learn_java/model/model.py
+++ learn_java/model/model.py
@@ -2,5 +2,6 @@ from model.java_handling import Java_Handling
 from model.chains import Chains
 
 class Model(Java_Handling, Chains):
+    # main model
     def __init__(self):
         super(Model,self).__init__()
