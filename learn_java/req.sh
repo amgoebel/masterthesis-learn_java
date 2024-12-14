0e7bb0f1f1c175073555e18eb07ada4f4e790e65
Alexander Goebel <agoebel@mailbox.org>
2024-12-14 18:14:01 +0100

    updated requirements

--- learn_java/req.sh
+++ learn_java/req.sh
@@ -1,8 +1,6 @@
-sudo apt update
-sudo apt install -y software-properties-common build-essential libffi-dev libssl-dev zlib1g-dev libncurses5-dev libncursesw5-dev libreadline-dev libsqlite3-dev libgdbm-dev libdb5.3-dev libbz2-dev libexpat1-dev liblzma-dev libffi-dev libssl-dev
 sudo add-apt-repository -y ppa:deadsnakes/ppa
-sudo apt install -y python3.12 python3.12-venv libxcb-cursor0
+sudo apt install -y software-properties-common build-essential libffi-dev libssl-dev zlib1g-dev libncurses5-dev libncursesw5-dev libreadline-dev libsqlite3-dev libgdbm-dev libdb5.3-dev libbz2-dev libexpat1-dev liblzma-dev libffi-dev libssl-dev python3.12 python3.12-venv libxcb-cursor0
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python3.12 get-pip.py
-python3.12 -m pip install pyqt6 bcrypt cryptography==44.0.0 python-dotenv langchain-openai langchain-community langchainhub cffi
-export OPENAI_API_KEY=sk-proj-0KMQeINNnl2EU3f50CLVT3BlbkFJTJqRvYtm3xgqS95M4Hh3
+python3.12 -m pip install pyqt6==6.7.1 bcrypt==4.2.1 cryptography==44.0.0 python-dotenv langchain-openai langchain-community langchainhub cffi==1.17.1
+echo "All requirements have been installed."
